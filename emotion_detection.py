6e227a7f20a9cf8cae4a19ec16d5dcb79b5db326
dangqn <nguyendq2005@gmail.com>
2025-03-01 18:47:56 -0500

    task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     "Send POST request to Emotion Predict of Watson NLP to get emotion from text_to_analyze"
@@ -6,4 +7,9 @@ def emotion_detector(text_to_analyse):
     headers = {'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en_stock'}
     data = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json=data, headers=headers)
-    return response.text
+    dict = json.loads(response.text)    
+    predict_emotions = dict['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(predict_emotions.keys(), key=predict_emotions.get)
+    predict_emotions['dominant_emotion'] = dominant_emotion
+    return predict_emotions
+
